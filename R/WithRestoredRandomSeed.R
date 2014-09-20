7ff1bb2be5febae53cc8f5f927e8000ce47a897d
Roy Lowrance <roy.lowrance@gmail.com>
2014-09-20 16:02:44 -0400

    new: run function then restore random seed

---
+++ R/WithRestoredRandomSeed.R
@@ -0,0 +1,7 @@
+WithRestoredRandomSeed <- function(f) {
+    # execute f() then restore the current random seed
+    oldseed <- NULL
+    if (exists('.Random.seed')) oldseed <- get('.Random.seed', .GlobalEnv)
+    f()
+    if (exists('.Random.seed')) assign('Random.seed', oldseed, .GlobalEnv)
+}
